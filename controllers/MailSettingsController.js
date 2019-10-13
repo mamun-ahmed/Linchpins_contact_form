87475cd7d4263e282b183e6f79330e81225c5f95
Mamun Ahmed <mamunahmed678@gmail.com>
2019-10-13 12:53:11 +0600

    console logged in mailSettings

--- controllers/MailSettingsController.js
+++ controllers/MailSettingsController.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 
 const MailSettingsController = {
     update: function(req, res){
-
+        
+        console.log(__dirname);
+        console.log(path.join(__dirname+'../data', 'data.json'));
         const rawData = fs.readFileSync('data/data.json');
         const data = JSON.parse(rawData);
 
